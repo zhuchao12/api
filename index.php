9222cfa7936c6079da9eab6606f2d0e988c663c5
zhuchao <479346874@qq.com>
2019-03-13 11:23:38 +0800

    asdasd

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+     $web_servers = [
+                     '188.131.238.133',
+                                     '188.131.177.111'
+                                             ];
+
+                       foreach($web_servers as $k=>$v){
+                       $cmd = 'ssh '.$v.'"cd /home/wwwroot/default/shop07 && git pull" ';
+                                    echo $cmd; echo '</br>';
+                                      $res = shell_exec($cmd);
+                                          echo $res; echo '</br>';
+                                 }
+?>
